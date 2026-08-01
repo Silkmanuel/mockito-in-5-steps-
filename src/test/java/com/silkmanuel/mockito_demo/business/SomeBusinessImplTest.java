8346a4216e1b3a0fca487b1849f882ec973641d9
Silkmanuel <107256054+Silkmanuel@users.noreply.github.com>
2026-08-01 15:55:03 +0100

    understanding problems with stubs

---
+++ src/test/java/com/silkmanuel/mockito_demo/business/SomeBusinessImplTest.java
@@ -0,0 +1,35 @@
+package com.silkmanuel.mockito_demo.business;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions.*;
+
+public class SomeBusinessImplTest {
+    @Test
+    void findTheGreatestFromAllData_basicScenario(){
+        SomeBusinessImpl businessImpl = new SomeBusinessImpl(new DataServiceStub1());
+        int result = businessImpl.findTheGreatestFromAllData();
+        Assertions.assertEquals(25, result);
+    }
+    
+    @Test
+    void findTheGreatestFromAllData_withOneValue(){
+        SomeBusinessImpl businessImpl = new SomeBusinessImpl(new DataServiceStub2());
+        int result = businessImpl.findTheGreatestFromAllData();
+        Assertions.assertEquals(35, result);
+    }
+}
+
+class DataServiceStub1 implements DataService {
+    @Override
+    public int[] retrieveAllData() {
+        return new int[]{25, 15, 5};
+    }
+}
+
+class DataServiceStub2 implements DataService {
+    @Override
+    public int[] retrieveAllData() {
+        return new int[]{35};
+    }
+}
